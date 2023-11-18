56824765e1c7c5fea9385022165ae67a0658f641
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-11-18 19:15:12 +0000

    user show follow/unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,15 @@ class UsersController < ApplicationController
     @username = params.fetch("username")
     @the_user = User.where(username: @username).first
 
+    matching_users = User.where({ :id => @the_user.id })
+
+    @user = matching_users.at(0)
+
+    matching_follow_requests = FollowRequest.where({ :sender_id => current_user.id, :recipient_id => @user.id })
+    the_follow_request = matching_follow_requests.at(0)
+
+    @followrequest = the_follow_request.present?
+
     if @the_user == nil
       redirect_to("/404")
     else
